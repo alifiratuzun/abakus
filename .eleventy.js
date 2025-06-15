2bf982266e94f66c57b457a5eb960807286524b3
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-15 15:52:56 +0200

    fix config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 module.exports = function (eleventyConfig) {
+  // Passthroughs
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("admin");
 
+  // Project collection
   eleventyConfig.addCollection("project", function (collection) {
     return collection.getFilteredByGlob("projects/*.md");
   });
 
+  // Directory structure
   return {
     dir: {
       input: ".",
