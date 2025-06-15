422576183da2e49f50cddcb0ff1f9dc7cff72f6d
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-15 17:43:23 +0200

    Fix Netlify Function to use native fetch and ESM

--- netlify/functions/github-auth.mjs
+++ netlify/functions/github-auth.mjs
@@ -1,11 +1,9 @@
-const fetch = require("node-fetch");
-
-exports.handler = async (event) => {
+export async function handler(event) {
     const client_id = process.env.GITHUB_CLIENT_ID;
     const client_secret = process.env.GITHUB_CLIENT_SECRET;
-
     const code = event.queryStringParameters.code;
-    const tokenRes = await fetch(`https://github.com/login/oauth/access_token`, {
+
+    const res = await fetch("https://github.com/login/oauth/access_token", {
         method: "POST",
         headers: { Accept: "application/json" },
         body: new URLSearchParams({
@@ -15,10 +13,10 @@ exports.handler = async (event) => {
         }),
     });
 
-    const token = await tokenRes.json();
+    const data = await res.json();
 
     return {
         statusCode: 200,
-        body: JSON.stringify(token),
+        body: JSON.stringify(data),
     };
-};
+}
