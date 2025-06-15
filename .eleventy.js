190dbe28c828971d7ea6f09e7032a2d700d045fe
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-16 00:53:52 +0200

    trying cms again40

--- .eleventy.js
+++ .eleventy.js
@@ -2,26 +2,28 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function (eleventyConfig) {
-  // Copy raw assets
+  // Passthrough static files
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("_redirects");
 
-  // Custom copy of admin/index.njk to _site/admin/index.html
-  eleventyConfig.on('afterBuild', () => {
-    const source = path.join(__dirname, 'admin/index.njk');
-    const destDir = path.join(__dirname, '_site/admin');
-    const dest = path.join(destDir, 'index.html');
+  // Custom copy: force-copy admin/index.njk to _site/admin/index.html
+  eleventyConfig.on("afterBuild", () => {
+    const source = path.join(__dirname, "admin/index.njk");
+    const destDir = path.join(__dirname, "_site/admin");
+    const dest = path.join(destDir, "index.html");
 
     if (!fs.existsSync(destDir)) {
       fs.mkdirSync(destDir, { recursive: true });
     }
 
     fs.copyFileSync(source, dest);
+    console.log("✔ admin/index.njk copied to _site/admin/index.html");
   });
 
+  // Custom collection for projects
   eleventyConfig.addCollection("project", function (collection) {
     return collection.getFilteredByGlob("projects/*.md");
   });
@@ -31,6 +33,6 @@ module.exports = function (eleventyConfig) {
       input: ".",
       output: "_site",
       includes: "_includes",
-    }
+    },
   };
 };
