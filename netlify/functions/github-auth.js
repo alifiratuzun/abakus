d241dc7c40fc04ee9f2dbd332c2dad6b6f75a198
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-15 19:00:39 +0200

    trying to fix cms

--- netlify/functions/github-auth.js
+++ netlify/functions/github-auth.js
@@ -6,16 +6,35 @@ exports.handler = async function (event) {
     const client_id = process.env.GITHUB_CLIENT_ID;
     const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
-    const accessTokenRes = await fetch(`https://github.com/login/oauth/access_token`, {
+    if (!code) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Missing code' }),
+        };
+    }
+
+    const response = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ client_id, client_secret, code })
+        body: JSON.stringify({
+            client_id,
+            client_secret,
+            code
+        })
     });
 
-    const data = await accessTokenRes.json();
+    const data = await response.json();
+
+    if (data.error) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify(data),
+        };
+    }
+
     return {
         statusCode: 200,
         body: JSON.stringify(data),
