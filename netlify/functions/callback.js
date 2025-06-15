ec06532e2f14053ab63abef3e8eab2ad360d4ee2
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-15 19:18:57 +0200

    trying to fix cms again2

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -1,18 +1,21 @@
-import fetch from 'node-fetch';
-
 exports.handler = async (event, context) => {
     const code = event.queryStringParameters.code;
 
     if (!code) {
-        return { statusCode: 400, body: 'Missing code' };
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Missing code' }),
+        };
     }
 
     const client_id = process.env.GITHUB_CLIENT_ID;
     const client_secret = process.env.GITHUB_CLIENT_SECRET;
 
-    const response = await fetch(`https://github.com/login/oauth/access_token`, {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
-        headers: { Accept: 'application/json' },
+        headers: {
+            Accept: 'application/json',
+        },
         body: new URLSearchParams({
             client_id,
             client_secret,
@@ -23,7 +26,10 @@ exports.handler = async (event, context) => {
     const data = await response.json();
 
     if (data.error) {
-        return { statusCode: 500, body: JSON.stringify(data) };
+        return {
+            statusCode: 500,
+            body: JSON.stringify(data),
+        };
     }
 
     return {
