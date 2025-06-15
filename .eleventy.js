51e53f41b798ba49a2c9e019ba94591721a694dc
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-16 00:51:05 +0200

    trying cms again39

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = function (eleventyConfig) {
+  // Copy raw assets
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("_redirects");
-  eleventyConfig.addPassthroughCopy("admin");
+
+  // Custom copy of admin/index.njk to _site/admin/index.html
+  eleventyConfig.on('afterBuild', () => {
+    const source = path.join(__dirname, 'admin/index.njk');
+    const destDir = path.join(__dirname, '_site/admin');
+    const dest = path.join(destDir, 'index.html');
+
+    if (!fs.existsSync(destDir)) {
+      fs.mkdirSync(destDir, { recursive: true });
+    }
+
+    fs.copyFileSync(source, dest);
+  });
 
   eleventyConfig.addCollection("project", function (collection) {
     return collection.getFilteredByGlob("projects/*.md");
@@ -14,7 +30,7 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: ".",
       output: "_site",
-      includes: "_includes"
+      includes: "_includes",
     }
   };
 };
