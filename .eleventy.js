914cdde3d44824a3f85c9090b2a169855668f859
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-22 17:06:19 +0200

    trying cms again47

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("_redirects");
+  eleventyConfig.addPassthroughCopy("admin/config.yml");
 
   eleventyConfig.addCollection("project", function (collection) {
     return collection.getFilteredByGlob("projects/*.md");
