9de21940fbc0cdaaeb4bee7cedb5f080d1824fab
Ali Firat Uzun <uzunalifirat@gmail.com>
2025-06-22 16:37:57 +0200

    trying cms again45

--- .eleventy.js
+++ .eleventy.js
@@ -4,9 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("_redirects");
-
-  // DO NOT copy admin/ — it's rendered via index.njk
-  // eleventyConfig.addPassthroughCopy("admin"); ← make sure this is NOT present
+  eleventyConfig.ignores.add("admin/index.html");
 
   eleventyConfig.addCollection("project", function (collection) {
     return collection.getFilteredByGlob("projects/*.md");
